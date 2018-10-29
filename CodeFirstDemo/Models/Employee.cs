c79dab00dc5ac76bb62af7c0b5383dfa15541cf9
Viken Patel <viken@promact.com>
2018-10-29 05:53:11 -0700

    First Commit

---
+++ CodeFirstDemo/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstDemo.Models
+{
+    public class Employee
+    {
+        public int StudentID { get; set; }
+    }
+}
